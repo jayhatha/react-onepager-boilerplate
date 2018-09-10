5121879be73d273138083fbf4f60018c5f922dde
jayhatha <jay@jayhathaway.com>
2018-09-10 12:06:55 -0700

    updated to current babel, build works

---
+++ webpack.config.js
@@ -0,0 +1,58 @@
+const HtmlWebPackPlugin = require("html-webpack-plugin");
+const path = require("path");
+const nodeExternals = require("webpack-node-externals");
+const client = {
+  entry: {
+    client: "./src/client/index.js"
+  },
+  target: "web",
+  output: {
+    filename: "[name].js",
+    path: path.resolve(__dirname, "dist/public")
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /(node_modules|bower_components)/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: ["@babel/preset-env"]
+          }
+        }
+      }
+    ]
+  },
+  plugins: [
+    new HtmlWebPackPlugin({
+      template: "./src/client/index.html"
+    })
+  ]
+};
+const server = {
+  entry: {
+    server: "./src/server/index.js"
+  },
+  target: "node",
+  output: {
+    filename: "[name].js",
+    path: path.resolve(__dirname, "dist")
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /(node_modules|bower_components)/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: ["@babel/preset-env"]
+          }
+        }
+      }
+    ]
+  },
+  externals: [nodeExternals()]
+};
+module.exports = [client, server];
