e272fbedfab543ef722946d316bff0d3d15bb826
jayhatha <jay@jayhathaway.com>
2018-09-10 12:26:11 -0700

    fixed jest with current babel, tests pass

---
+++ src/client/app.test.js
@@ -0,0 +1,10 @@
+import App from './app'
+import React from 'react'
+import {shallow} from 'enzyme'
+describe('App', () => {
+  test('should match snapshot', () => {
+    const wrapper = shallow(<App/>)
+    expect(wrapper.find('div').text()).toBe('Welcome to React Boilerplate App')
+    expect(wrapper).toMatchSnapshot()
+  })
+})
